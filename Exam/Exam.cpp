8e72c06fc07a0a75637fbc383c3f07cf0087cbe6
Yana Lehka <136910384+userlozhka@users.noreply.github.com>
2023-12-03 17:54:32 +0200

    Exam.cpp

--- Exam/Exam.cpp
+++ Exam/Exam.cpp
@@ -1,25 +1,30 @@
 ﻿#include <iostream>
-#include <vector>
 
-// Функція для знаходження номеру максимального елемента масиву
-int findMaxIndex(const std::vector<int>& arr) {
+// Function to find the index of the maximum element in the array
+int findMaxIndex(const int arr[], int size) {
+    int maxElement = arr[0];
     int maxIndex = 0;
-    for (int i = 1; i < arr.size(); ++i) {
-        if (arr[i] > arr[maxIndex]) {
+
+    for (int i = 1; i < size; ++i) {
+        if (arr[i] > maxElement) {
+            maxElement = arr[i];
             maxIndex = i;
         }
     }
+
     return maxIndex;
 }
 
-// Функція для обчислення добутку елементів масиву між першим і другим нульовими елементами
-int productBetweenZeros(const std::vector<int>& arr) {
-    int firstZeroIndex = -1, secondZeroIndex = -1;
-    for (int i = 0; i < arr.size(); ++i) {
+// Function to find the product of elements between the first and second zero elements
+int productBetweenZeros(const int arr[], int size) {
+    int firstZeroIndex = -1;
+    int secondZeroIndex = -1;
+    int product = 1;
+
+    for (int i = 0; i < size; ++i) {
         if (arr[i] == 0) {
-            if (firstZeroIndex == -1) {
+            if (firstZeroIndex == -1)
                 firstZeroIndex = i;
-            }
             else {
                 secondZeroIndex = i;
                 break;
@@ -27,47 +32,50 @@ int productBetweenZeros(const std::vector<int>& arr) {
         }
     }
 
-    if (firstZeroIndex == -1 || secondZeroIndex == -1) {
-        std::cout << "Немає двох нульових елементів у масиві.\n";
+    if (firstZeroIndex != -1 && secondZeroIndex != -1) {
+        for (int i = firstZeroIndex + 1; i < secondZeroIndex; ++i) {
+            product *= arr[i];
+        }
+    }
+    else {
+        // If there are less than two zeros, return 0 or choose another default value
         return 0;
     }
 
-    int product = 1;
-    for (int i = firstZeroIndex + 1; i < secondZeroIndex; ++i) {
-        product *= arr[i];
-    }
     return product;
 }
 
 int main() {
-    std::vector<int> array = { 1, 3, 0, 5, 7, 2, 4, 0, 6, 8 };
+    const int size = 10;
+    int array[size] = { 3, 7, 2, 0, 4, 6, 0, 9, 5, 8 };
 
-    // Знаходження номеру максимального елемента
-    int maxIndex = findMaxIndex(array);
-    std::cout << "Номер максимального елемента: " << maxIndex << std::endl;
+    int maxIndex = findMaxIndex(array, size);
+    std::cout << "Index of maximum element: " << maxIndex << std::endl;
 
-    // Обчислення добутку елементів між першим і другим нульовими елементами
-    int product = productBetweenZeros(array);
-    std::cout << "Добуток елементів між нулями: " << product << std::endl;
+    int product = productBetweenZeros(array, size);
+    std::cout << "Product of elements between the first and second zeros: " << product << std::endl;
 
-    // Перетворення масиву
-    std::vector<int> transformedArray(array.size());
-    int oddIndex = 0, evenIndex = array.size() / 2;
-    for (int i = 0; i < array.size(); ++i) {
-        if (i % 2 == 0) {
-            transformedArray[evenIndex] = array[i];
-            evenIndex++;
-        }
-        else {
-            transformedArray[oddIndex] = array[i];
-            oddIndex++;
-        }
+    // Transforming the array
+    int temp[size];
+    int oddIndex = 0;
+    int evenIndex = size / 2;
+
+    for (int i = 0; i < size; ++i) {
+        if (i % 2 == 0)
+            temp[oddIndex++] = array[i];
+        else
+            temp[evenIndex++] = array[i];
     }
 
-    // Виведення перетвореного масиву
-    std::cout << "Перетворений масив: ";
-    for (const auto& elem : transformedArray) {
-        std::cout << elem << " ";
+    // Copying back to the original array
+    for (int i = 0; i < size; ++i) {
+        array[i] = temp[i];
+    }
+
+    // Displaying the modified array
+    std::cout << "Transformed array: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << array[i] << " ";
     }
     std::cout << std::endl;
 
